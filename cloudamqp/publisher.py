a3cc3e0f0a93e599846f0c1e1250a7c9513298b3
joao_pedro250 <jpedrobarbosa33@gmail.com>
2022-11-26 19:12:20 -0300

    corrigindo erro nome funcção

--- cloudamqp/publisher.py
+++ cloudamqp/publisher.py
@@ -6,7 +6,7 @@ connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
 channel.exchange_declare('text_exchange')
-channel.queue_declare(queu='test_queue')
+channel.queue_declare(queue='test_queue')
 channel.queue_bind('test_queue', 'test_exchange', 'tests')
 
 channel.basic_publish(
